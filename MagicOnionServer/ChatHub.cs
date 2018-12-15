7bbaa4508b54b42c31cf84246fb3c0b7213b99f6
mitchydeath <michiwaki.shohei@gmail.com>
2018-12-16 03:55:40 +0900

    コメント追加

--- MagicOnionServer/ChatHub.cs
+++ MagicOnionServer/ChatHub.cs
@@ -12,8 +12,21 @@ namespace MagicOnionServer
     /// </summary>
     public interface IChatHubReceiver
     {
+        /// <summary>
+        /// 誰かがチャットに参加したことをクライアントに伝える。
+        /// </summary>
+        /// <param name="name">参加した人の名前</param>
         void OnJoin(string name);
+        /// <summary>
+        /// 誰かがチャットから退室したことをクライアントに伝える。
+        /// </summary>
+        /// <param name="name">退室した人の名前</param>
         void OnLeave(string name);
+        /// <summary>
+        /// 誰かが発言した事をクライアントに伝える。
+        /// </summary>
+        /// <param name="name">発言した人の名前</param>
+        /// <param name="message">メッセージ</param>
         void OnSendMessage(string name, string message);
     }
 
@@ -22,7 +35,16 @@ namespace MagicOnionServer
     /// </summary>
     public interface IChatHub : IStreamingHub<IChatHub, IChatHubReceiver>
     {
+        /// <summary>
+        /// 参加することをサーバに伝える
+        /// </summary>
+        /// <param name="userName">参加者の名前</param>
+        /// <returns></returns>
         Task JoinAsync(string userName);
+        /// <summary>
+        /// 退室することをサーバに伝える
+        /// </summary>
+        /// <returns></returns>
         Task LeaveAsync();
         Task SendMessageAsync(string message);
     }
@@ -34,22 +56,28 @@ namespace MagicOnionServer
 
         public async Task JoinAsync(string userName)
         {
-            //ルーム名は固定
+            //ルームは全員固定
             const string roomName = "SampleRoom";
+            //ルームに参加&ルームを保持
             this.room = await this.Group.AddAsync(roomName);
+            //自分の名前も保持
             me = userName;
+            //参加したことをルームに参加している全メンバーに通知
             this.Broadcast(room).OnJoin(userName);
         }
 
         public async Task LeaveAsync()
         {
+            //ルーム内のメンバーから自分を削除
             await room.RemoveAsync(this.Context);
+            //退室したことを全メンバーに通知
             this.Broadcast(room).OnLeave(me);
         }
 
 
         public async Task SendMessageAsync(string message)
         {
+            //発言した内容を全メンバーに通知
             this.Broadcast(room).OnSendMessage(me, message);
         }
 
